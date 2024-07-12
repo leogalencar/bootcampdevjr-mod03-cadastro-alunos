e986fbee1c1072410459d7c41be2b9d9996d0f70
leogalencar <leonardogalencar444@gmail.com>
2024-07-12 13:11:55 -0300

    closes #7 - Endpoint GET /courses

--- backend/src/main/java/com/abutua/backend/controllers/CourseController.java
+++ backend/src/main/java/com/abutua/backend/controllers/CourseController.java
@@ -3,6 +3,8 @@ package com.abutua.backend.controllers;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.abutua.backend.models.Course;
@@ -10,4 +12,9 @@ import com.abutua.backend.models.Course;
 @RestController
 public class CourseController {
     private List<Course> courses = Arrays.asList();
+
+    @GetMapping("courses")
+    public ResponseEntity<List<Course>> getCourses() {
+        return ResponseEntity.ok(courses);
+    }
 }
