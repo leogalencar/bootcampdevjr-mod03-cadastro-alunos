eca92c265273fd2086df254fd3b0aba15de1690d
leogalencar <leonardogalencar444@gmail.com>
2024-07-12 13:10:25 -0300

    closes #6 - Endpoint GET /students/{id}

--- backend/src/main/java/com/abutua/backend/controllers/StudentController.java
+++ backend/src/main/java/com/abutua/backend/controllers/StudentController.java
@@ -3,9 +3,12 @@ package com.abutua.backend.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.abutua.backend.models.Student;
 
@@ -16,5 +19,14 @@ public class StudentController {
     @GetMapping("students")
     public ResponseEntity<List<Student>> getStudents() {
         return ResponseEntity.ok(students);
+    }
+
+    @GetMapping("students/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable int id) {
+        Student student = students.stream().filter(s -> s.getId() == id).findFirst().orElseThrow(
+            () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Student Not Found")
+        );
+
+        return ResponseEntity.ok(student);
     }
 }
